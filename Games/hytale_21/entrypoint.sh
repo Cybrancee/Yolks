332c2fc05c3bd2e86ef30eee06fd6e14c9f4247e
YoloFTW <andretoddriddle@hotmail.com>
2026-07-07 14:06:43 +0100

    Added hytale

---
+++ Games/hytale_21/entrypoint.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+cd /home/container
+
+# Make internal Docker IP address available to processes.
+INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
+export INTERNAL_IP
+
+# Print Java Version
+java -version
+
+
+# Get Hytale token
+if [[ -z "$HYTALE_SERVER_SESSION_TOKEN" || -z "$HYTALE_SERVER_IDENTITY_TOKEN" ]]; then
+
+    URL="https://hytale.cybrancee.com/token"
+
+    RESPONSE=$(curl -sS "$URL")
+
+    if [ $? -eq 0 ]; then
+        
+        s_token=$(echo "$RESPONSE" | jq -r '.sessionToken')
+        i_token=$(echo "$RESPONSE" | jq -r '.identityToken')
+
+        # validate tokens arent empty
+        if [[ "$s_token" != "null" && -n "$s_token" && "$i_token" != "null" && -n "$i_token" ]]; then
+            export HYTALE_SERVER_SESSION_TOKEN="$s_token"
+            export HYTALE_SERVER_IDENTITY_TOKEN="$i_token"
+            echo "Tokens exported successfully."
+        else
+            echo "Warning: API response was valid but tokens were missing or null."
+        fi
+
+    else
+        echo "Warning: Network request failed. No variables were set."
+    fi
+else
+    echo "Tokens are already set. Skipping API request."
+fi
+
+# Replace Startup Variables
+MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
+echo ":/home/container$ ${MODIFIED_STARTUP}"
+
+# Run the Server
+eval ${MODIFIED_STARTUP}
